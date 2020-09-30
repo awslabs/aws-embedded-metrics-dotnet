ac71f1eb1898240b0a637272a5c11f045e733dc8
cmobrien <cmobrien@godaddy.com>
2020-09-30 11:19:40 -0700

    stubbed methods for DefaultEnvironment

--- Amazon.CloudWatch.EMF/Environment/DefaultEnvironment.cs
+++ Amazon.CloudWatch.EMF/Environment/DefaultEnvironment.cs
@@ -1,6 +1,26 @@
+using Amazon.CloudWatch.EMF.Model;
+using Amazon.CloudWatch.EMF.Sink;
+
 namespace Amazon.CloudWatch.EMF.Environment
 {
-    public class DefaultEnvironment
+    public class DefaultEnvironment : IEnvironment
     {
+        public string Name => throw new System.NotImplementedException();
+
+        public string Type => throw new System.NotImplementedException();
+
+        public string LogGroupName => throw new System.NotImplementedException();
+
+        public ISink Sink => throw new System.NotImplementedException();
+
+        public void ConfigureContext(MetricsContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Probe()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
