d07734dd024fe1539e79ea68c0165f67d1ffc144
Nirmala Tatikonda <ntatikonda@godaddy.com>
2020-09-30 08:38:09 -0700

    Added implementation

---
+++ Amazon.CloudWatch.EMF/Constants.cs
@@ -0,0 +1,11 @@
+namespace Amazon.CloudWatch.EMF
+{
+    public class Constants
+    {
+        public const int DEFAULT_AGENT_PORT = 25888;
+
+        public const string UNKNOWN = "Unknown";
+
+        public const int MAX_METRICS_PER_EVENT = 100;
+    }
+}
