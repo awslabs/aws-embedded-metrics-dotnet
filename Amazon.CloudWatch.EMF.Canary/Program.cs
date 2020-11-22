7eda10f0a534cc516f52b456958430ee355e22b3
Jared Nance <jaredcnance@gmail.com>
2020-11-22 16:17:06 -0600

    fix memory leak in canary

--- Amazon.CloudWatch.EMF.Canary/Program.cs
+++ Amazon.CloudWatch.EMF.Canary/Program.cs
@@ -22,7 +22,7 @@ namespace Amazon.CloudWatch.EMF.Canary
             var configuration = new Configuration
             {
                 LogGroupName = "/Canary/Dotnet/CloudWatchAgent/Metrics",
-                EnvironmentOverride = Environments.ECS,
+                EnvironmentOverride = Environments.Local,
                 AgentEndPoint = "tcp://cloudwatch-agent:25888"
             };
 
@@ -35,33 +35,33 @@ namespace Amazon.CloudWatch.EMF.Canary
 
             while (true)
             {
-                var logger = new MetricsLogger(loggerFactory);
-                logger.SetNamespace("Canary");
-
-                var dimensionSet = new DimensionSet();
-                dimensionSet.AddDimension("Runtime", "Dotnet");
-                dimensionSet.AddDimension("Platform", "ECS");
-                dimensionSet.AddDimension("Agent", "CloudWatchAgent");
-                dimensionSet.AddDimension("Version", version);
-                logger.SetDimensions(dimensionSet);
-
-                using (Process currentProcess = System.Diagnostics.Process.GetCurrentProcess())
+                using (var logger = new MetricsLogger(loggerFactory)) 
                 {
-                    //https://github.com/dotnet/corefx/blob/3633ea2c6bf9d52029681efeedd84fd7a06eb6ba/src/System.Diagnostics.Process/src/System/Diagnostics/ProcessManager.Linux.cs#L137
-                    logger.PutMetric("Memory.RSS", currentProcess.WorkingSet64, Unit.BYTES);
+                    logger.SetNamespace("Canary");
+
+                    var dimensionSet = new DimensionSet();
+                    dimensionSet.AddDimension("Runtime", "Dotnet");
+                    dimensionSet.AddDimension("Platform", "ECS");
+                    dimensionSet.AddDimension("Agent", "CloudWatchAgent");
+                    dimensionSet.AddDimension("Version", version);
+                    logger.SetDimensions(dimensionSet);
+
+                    using (var currentProcess = System.Diagnostics.Process.GetCurrentProcess())
+                    {
+                        //https://github.com/dotnet/corefx/blob/3633ea2c6bf9d52029681efeedd84fd7a06eb6ba/src/System.Diagnostics.Process/src/System/Diagnostics/ProcessManager.Linux.cs#L137
+                        logger.PutMetric("Memory.RSS", currentProcess.WorkingSet64, Unit.BYTES);
+                    }
+
+                    logger.PutMetric("Invoke", 1, Unit.NONE);
+
+                    if (init) {
+                        init = false;
+                        logger.PutMetric("Init", 1, Unit.NONE);
+                    }
+
+                    logger.PutMetric("Memory.HeapUsed", GC.GetTotalMemory(false), Unit.BYTES);
                 }
-
-                logger.PutMetric("Invoke", 1, Unit.NONE);
-
-                if (init) {
-                    init = false;
-                    logger.PutMetric("Init", 1, Unit.NONE);
-                }
-
-                logger.PutMetric("Memory.HeapUsed", GC.GetTotalMemory(false), Unit.BYTES);
-
-                logger.Flush();
-                Thread.Sleep(30_000);
+                Thread.Sleep(1_000);
             }
         }
     }
